09cec3ff1968385c25693d61cf43832dfe4fea74
Melisha <melisha.evans06@gmail.com>
2023-08-29 18:20:49 -0700

    update seeds.sql and index.js

--- index.js
+++ index.js
@@ -2,3 +2,40 @@ const { prompt } = require("inquirer");
 const db = require("./db");
 
 init();
+
+function loadPrompts() {
+    prompt([
+        {
+            type: "",
+            name: "",
+            message: ""
+        }
+    ])
+}
+
+function viewRoles() {
+    db.findRoles()
+}
+
+
+function viewDepartments() {
+    db.findDepartments()
+}
+
+function addEmployee() {
+    prompt([
+        {
+            name: "first_name",
+            message: "What is their first name?"
+        },
+        {
+            name: "last_name",
+            message: "What is their last name?"
+        }
+    ])
+}
+
+function quit() {
+    console.log("Fare thee well!");
+    process.exit();
+}
